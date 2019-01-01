36acd341ed0484a8112be9aebf49422fc13d16e4
bill <bill.yang@dingtone.me>
2019-01-01 19:15:34 +0800

    AddTwoNumbers

---
+++ app/src/main/java/yangxinwu/algorithm/AddTwoNumbers.java
@@ -0,0 +1,120 @@
+package yangxinwu.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AddTwoNumbers {
+
+
+    /**
+     *https://leetcode-cn.com/problems/add-two-numbers/
+     *
+     *
+     * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+
+     如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+
+     您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+     示例：
+
+     输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     输出：7 -> 0 -> 8
+     原因：342 + 465 = 807
+     *
+     *
+     *
+     */
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.value : 0;
+            int y = (q != null) ? q.value : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+
+    public static class ListNode{
+        int value;
+        ListNode next;
+        public ListNode(int value){
+            this.value = value;
+        }
+    }
+
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.value) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode l1 = stringToListNode(line);
+            line = in.readLine();
+            ListNode l2 = stringToListNode(line);
+
+            ListNode ret = new AddTwoNumbers().addTwoNumbers(l1, l2);
+
+            String out = listNodeToString(ret);
+
+            System.out.print(out);
+        }
+    }
+
+}
