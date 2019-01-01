756f905dfad97bbcbfde9669c4ae3f4e626ca721
bill <bill.yang@dingtone.me>
2019-01-01 17:06:40 +0800

    GenerateParentheses

--- app/src/main/java/yangxinwu/algorithm/GenerateParentheses.java
+++ app/src/main/java/yangxinwu/algorithm/GenerateParentheses.java
@@ -3,6 +3,28 @@ package yangxinwu.algorithm;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ *
+ * https://leetcode-cn.com/problems/generate-parentheses/
+ *
+ *    给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+
+ 例如，给出 n = 3，生成结果为：
+
+ [
+ "((()))",
+ "(()())",
+ "(())()",
+ "()(())",
+ "()()()"
+ ]
+
+ *
+ *
+ *
+ */
+
 public class GenerateParentheses {
 
 
